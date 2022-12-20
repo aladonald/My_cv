1472c4af85ac006f60ebd5cde164a7236a2ec80a
Alan Donald <alandonaldhp@gmail.com>
2022-12-20 12:28:56 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,78 @@
+// import "https://flackr.github.io/scroll-timeline/dist/scroll-timeline.js";
+// 'use strict'
+// const skills = document.getElementById("skills");
+// const experience = document.querySelector(".exp");
+
+// const scrollTracking = new ScrollTimeline({
+
+//     scrollOffsets : [{
+//         target: skills, edge: "end", threshold: "0"
+        
+//     },
+//     {
+//         target: skills, edge: "start", threshold: "1"
+//     }
+// ],
+// });
+
+// const expScroll = new ScrollTimeline({
+// scrollOffsets:[{
+//     target: experience, edge:"end", threshold:"0"
+// }
+// ,
+// {
+//     target: experience, edge:"start", threshold:"30rem"
+
+// }]
+// })
+
+// experience.animate({
+//     transform:[
+//         "scaleX(0)","scaleX(1)"
+//     ]
+// },
+// {
+//     duration: 1,
+//     timeline: expScroll,
+// })
+
+//=====================================sticky navbar======================================//
+
+'use strict'
+const section2 = document.querySelector(".experience")
+const sec2Coords = section2.getBoundingClientRect()
+const nav = document.querySelector(".nav")
+const navLi = document.querySelector(".nav ul li a")
+window.addEventListener("scroll", function(){
+    if(window.scrollY > sec2Coords.top){
+        nav.classList.add("sticky")
+    }else{
+        nav.classList.remove("sticky")
+    }
+})
+
+//=============================show image while scrolling===============================//
+
+const allSection = document.querySelectorAll(".section")
+
+const revealSection = function(entries, observer){
+     const[entry] = entries;
+    console.log(entry)
+    if(!entry.isIntersecting) return;
+
+    entry.target.classList.remove("sectionhidden")
+    observer.unobserve(entry.target)
+}
+
+const sectionObserver = new IntersectionObserver(
+    revealSection, {
+        root:null,
+        threshold:.30,
+
+    }
+)
+
+allSection.forEach(function(section){
+    sectionObserver.observe(section);
+    section.classList.add("sectionhidden")
+})
